dbbdbeff3f74ff10afa6a49b951df4fc9608210c
Maciej Manna <maciej.manna@gmail.com>
2020-01-04 18:17:11 +0100

    initial project setup

--- se-exp-win32-d3d11/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-
-// int main() { return 0; }
-
-INT WINAPI WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PSTR lpCmdLine, _In_ INT nCmdShow)
-{
-	return 0;
-}
